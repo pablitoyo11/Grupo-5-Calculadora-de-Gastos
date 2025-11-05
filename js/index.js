ac6cee8e7f37c56a6aa5aaa960068e10ee8f3adc
pablitoyo11 <pablo_lo_mas_k@hotmail.com>
2025-11-05 14:42:37 -0300

    Agregar gastos en una "ventana emergente" y errores de tipeo

--- js/index.js
+++ js/index.js
@@ -1,38 +1,64 @@
-//seccion formulario de gasto
-const formulario = document.getElementById("formularioCotizacion");
+//formulario de gasto
+const formularioGastos = document.getElementById("formularioGastos");
 const arrayDeGastos = [];
+//contenedor para el resumen de gastos
+const resumenGastos = document.getElementById("resumenGastos");
 
-formulario.addEventListener("submit", function (e) {
+//.value presupuesto maximo
+const inputPresupuestoMaxElement = document.getElementById(
+  "inputPresupuestoMax"
+);
+
+//bloque invisible emergente para agregar gastos
+const btnAbrirAgregarGastosModal = document.getElementById(
+  "btnAbrirAgregarGastosModal"
+);
+const agregarGastosModal = document.getElementById("agregarGastosModal");
+const btnCerrarAgregarGastosModal = document.getElementById(
+  "btnCerrarAgregarGastosModal"
+);
+btnAbrirAgregarGastosModal.addEventListener("click", () => {
+  agregarGastosModal.style.display = "block"; //mostrar el modal
+});
+btnCerrarAgregarGastosModal.addEventListener("click", () => {
+  agregarGastosModal.style.display = "none"; //ocultar el modal
+});
+
+formularioGastos.addEventListener("submit", function (e) {
   e.preventDefault();
+  let inputPresupuestoMax = inputPresupuestoMaxElement.value;
+  if (inputPresupuestoMax <= 0) {
+    alert(
+      "Por favor, ingrese un presupuesto maximo para mejorar la experiencia"
+    );
+    return;
+  }
   const gastoCategoria = document.getElementById("gastoCategoria").value;
   const gastoMonto = document.getElementById("gastoMonto").value;
   if (!gastoCategoria || !gastoMonto) {
-    alert("Por favor, complete todos los campos correctamente!");
+    alert("Por favor, complete todos los campos correctamente!"); //esto no se activa porque el form ya pide que se complete antes del submit? como evitar?
   } else {
     arrayDeGastos.push({
       categoria: gastoCategoria,
       monto: parseFloat(gastoMonto),
     });
+    calcularTotalGastos();
   }
 });
 
 //funcion para calcular el total de gastos
 function calcularTotalGastos() {
-  const inputPresupuestoMax = document.getElementById(
-    "inputPresupuestoMax"
-  ).value;
-  const montoTotalDeGastos = 0;
-  montoTotalDeGastos = arrayDeGastos.forEach(
-    (gasto) => (monstoTotal += gasto.monto)
-  );
+  let montoTotalDeGastos = 0;
+  let inputPresupuestoMax = inputPresupuestoMaxElement.value;
+  arrayDeGastos.forEach((gasto) => (montoTotalDeGastos += gasto.monto));
 
-  const promedioDeGastos = montoTotalDeGastos / arrayDeGastos.length;
+  let promedioDeGastos = montoTotalDeGastos / arrayDeGastos.length;
 
-  const arrayHistorialGastosHtml = "";
+  let arrayToHtmlHistorialGastos = "";
   for (let index = 0; index < arrayDeGastos.length; index++) {
-    arrayHistorialGastosHtml = +`
+    arrayToHtmlHistorialGastos += `
     <p>
-      <strong>Categoria: </strong>${arrayDeGastos[index].categoria}{" "}
+      <strong>Categoria: </strong>${arrayDeGastos[index].categoria}
       <strong>Monto: </strong> ${arrayDeGastos[index].monto}
     </p>;
     `;
@@ -40,18 +66,14 @@ function calcularTotalGastos() {
 
   resumenGastos.innerHTML = `
         <h3>Resumen Gastos Del Mes</h3>
-        <p><strong>Presupuesto Maximo:</strong>${inputPresupuestoMax}</p>
-        <p><strong>Monto total de Gastos:</strong>${montoTotalDeGastos}</p>
-        <p><strong>Promedio de gastos:</strong>${promedioDeGastos}</p>
-        <p><strong>Restante</strong>USD ${
+        <p><strong>Presupuesto Maximo:</strong> $ ${inputPresupuestoMax}</p>
+        <p><strong>Monto total de Gastos:</strong> $ ${montoTotalDeGastos}</p>
+        <p><strong>Promedio de gastos:</strong> $ ${promedioDeGastos}</p>
+        <p><strong>Restante</strong> $ ${
           inputPresupuestoMax - montoTotalDeGastos
         }</p>
 
-        <p><strong>Historial de gastos:</strong>${arrayHistorialGastosHtml}</p>
-        
-
-        <hr>
-        <h2 style="color: #007bff">Prima estimada: USD ${primaFinal.toLocaleString()}</h2>
-        <p style="font-size: 13px; color: #555">*Esta es una cotización estimada. El valor puede variar según la compañia aseguradora.</p>
+        <p><strong>Historial de gastos:</strong>${arrayToHtmlHistorialGastos}</p>
+      
     `;
 }
