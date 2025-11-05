1ea230391112f6118dbd807e0ac417ab516a7e0d
pablitoyo11 <pablo_lo_mas_k@hotmail.com>
2025-11-05 12:23:45 -0300

    array para adicionar html

--- js/index.js
+++ js/index.js
@@ -28,11 +28,14 @@ function calcularTotalGastos() {
 
   const promedioDeGastos = montoTotalDeGastos / arrayDeGastos.length;
 
+  const arrayHistorialGastosHtml = "";
   for (let index = 0; index < arrayDeGastos.length; index++) {
+    arrayHistorialGastosHtml = +`
     <p>
       <strong>Categoria: </strong>${arrayDeGastos[index].categoria}{" "}
       <strong>Monto: </strong> ${arrayDeGastos[index].monto}
     </p>;
+    `;
   }
 
   resumenGastos.innerHTML = `
@@ -40,12 +43,13 @@ function calcularTotalGastos() {
         <p><strong>Presupuesto Maximo:</strong>${inputPresupuestoMax}</p>
         <p><strong>Monto total de Gastos:</strong>${montoTotalDeGastos}</p>
         <p><strong>Promedio de gastos:</strong>${promedioDeGastos}</p>
-
-        <p><strong>Historial de gastos:</strong>${cobertura.toUpperCase()}</p>
-        
         <p><strong>Restante</strong>USD ${
           inputPresupuestoMax - montoTotalDeGastos
         }</p>
+
+        <p><strong>Historial de gastos:</strong>${arrayHistorialGastosHtml}</p>
+        
+
         <hr>
         <h2 style="color: #007bff">Prima estimada: USD ${primaFinal.toLocaleString()}</h2>
         <p style="font-size: 13px; color: #555">*Esta es una cotización estimada. El valor puede variar según la compañia aseguradora.</p>
